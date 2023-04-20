f07c726b06895848ad956a43eb59fe230522cfaa
meganf247 <97106657+meganf247@users.noreply.github.com>
2023-04-20 13:54:28 -0400

    Update VACUUM.sql

--- VACUUM.sql
+++ VACUUM.sql
@@ -1,4 +1,4 @@
-/* table_name: the name of the table you wish to vacuum; num: the retention threshold /*
+/* table_name: the name of the table you wish to vacuum; num: the retention threshold */
 
 VACUUM {{ table_name }}
 RETAIN {{ num }} HOURS
