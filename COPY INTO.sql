ab2f31053115147a8abc8da39bc5e7c2fe858679
meganf247 <97106657+meganf247@users.noreply.github.com>
2023-04-20 13:54:13 -0400

    Update COPY INTO.sql

--- COPY INTO.sql
+++ COPY INTO.sql
@@ -1,7 +1,13 @@
+/* table_name: existing delta table; 
+source: the file location to load the data from (ex: S3 bucket path); 
+file_format: the format of the source file(s); 
+format_options: options to be passed to the Spark data source reader for the specified format; 
+copy_options: options to control the operation of the COPY INTO command  */ 
+
 CREATE TABLE IF NOT EXISTS {{ table_name }};
 
 COPY INTO {{ table_name }}
-FROM {{ S3_bucket_path }}
-FILEFORMAT = {{ File_Format }}
+FROM {{ source }}
+FILEFORMAT = {{ file_format }}
 FORMAT OPTIONS ({{ format_options }})
 COPY_OPTIONS ({{ copy_options }})
