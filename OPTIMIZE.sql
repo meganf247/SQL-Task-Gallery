3e08b98b1d94d7ee08df26bc6941c57198340c64
meganf247 <97106657+meganf247@users.noreply.github.com>
2023-04-20 13:56:45 -0400

    Update OPTIMIZE.sql

--- OPTIMIZE.sql
+++ OPTIMIZE.sql
@@ -1,2 +1,6 @@
+/* table_name: name of existing Delta table;
+predicate: (optiona) optimize a subset of rows matching the given partition predicate
+column_name_list: (optional) colocate column information in the same set of files */
+
 OPTIMIZE {{ table_name }} WHERE {{ predicate }}
     ZORDER BY ({{ column_name_list }})
