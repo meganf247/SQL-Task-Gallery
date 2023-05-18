82c39e237312fa1a10b14667326b11bbac197604
meganf247 <97106657+meganf247@users.noreply.github.com>
2023-05-18 10:47:36 -0400

    Update NOT NULL_constraint.sql

--- NOT NULL_constraint.sql
+++ NOT NULL_constraint.sql
@@ -1,2 +1,7 @@
+/* indicates that values in specific columns cannot be null.
+table_name: existing delta table; 
+column_a: the column to apply the NOT NULL constraint to */ 
+
+
 ALTER TABLE {{ table_name }}
 ADD CONSTRAINT {{ column_a }} IS NOT NULL;
