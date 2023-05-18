09c8961cd5ce189f4ce6d0f6ed9203eaebcd1dd6
meganf247 <97106657+meganf247@users.noreply.github.com>
2023-05-18 10:49:18 -0400

    Update add_columns.sql

--- add_columns.sql
+++ add_columns.sql
@@ -1,2 +1,8 @@
+/* add columns to a delta table
+table_name: existing delta table; 
+col_name1 and col_name2: names of the columns to add
+data_type1 and data_type2: the data type of col_name1 and col_name2 respectively*/ 
+
+
 ALTER TABLE {{ table_name }} ADD COLUMNS ({{ col_name1 }} {{ data_type1 }},
 {{ col_name2 }} {{ data_type2 }});
